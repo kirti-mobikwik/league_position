24035b70b66f0d86fe6ca1af8d5812cf27d127a5
Kirti Mobikwik <kirti.nagpal@mobikwik.com>
2021-01-08 19:14:56 +0530

    config added

--- footballleague/src/main/java/com/mobikiwik/league/service/LeagueService.java
+++ footballleague/src/main/java/com/mobikiwik/league/service/LeagueService.java
@@ -2,6 +2,7 @@ package com.mobikiwik.league.service;
 
 import com.mobikiwik.league.model.League;
 import com.mobikiwik.league.model.LeagueComparator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -16,11 +17,15 @@ import java.util.List;
 
 @Service
 public class LeagueService {
+    @Value("${league.api}")
+    private String api;
+    @Value("${league.url}")
+    private String url;
     public List<League> getLeagues(LeagueComparator leagueComparator){
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers =  new HttpHeaders();
         headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://apiv2.apifootball.com/?action=get_leagues&country_id="+leagueComparator.getCountryId()+"&").queryParam("APIkey","9bb66184e0c8145384fd2cc0f7b914ada57b4e8fd2e4d6d586adcc27c257a978");
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url+leagueComparator.getCountryId()+"&").queryParam("APIkey",api);
         List<League> leagueList = Arrays.asList(restTemplate.exchange(builder.toUriString(), HttpMethod.POST, new HttpEntity<>(headers), League[].class).getBody());
         return leagueList;
     }
