1229175eb4ed7b5264c65457f41cf6585b6f00cb
Kirti Mobikwik <kirti.nagpal@mobikwik.com>
2021-01-08 19:14:43 +0530

    config added

--- footballcountry/src/main/java/com/mobikwik/country/service/CountryService.java
+++ footballcountry/src/main/java/com/mobikwik/country/service/CountryService.java
@@ -3,6 +3,7 @@ package com.mobikwik.country.service;
 
 import com.mobikwik.country.model.Country;
 import com.mobikwik.country.model.CountryComparator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -16,12 +17,16 @@ import java.util.List;
 
 @Service
 public class CountryService {
+    @Value("${country.api}")
+    private String api;
+    @Value("${country.url}")
+    private String url;
 
     public List<Country> getCountries(){
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers =  new HttpHeaders();
         headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://apiv2.apifootball.com/?action=get_countries&").queryParam("APIkey","9bb66184e0c8145384fd2cc0f7b914ada57b4e8fd2e4d6d586adcc27c257a978");
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url).queryParam("APIkey",api);
         List<Country> countryList = Arrays.asList(restTemplate.exchange(builder.toUriString(), HttpMethod.POST, new HttpEntity<>(headers), Country[].class).getBody());
         return countryList;
     }
