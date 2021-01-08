f8dde41fd56396904f54241ce78a2ada39aa48dc
Kirti Mobikwik <kirti.nagpal@mobikwik.com>
2021-01-08 19:14:25 +0530

    config added

--- football/src/main/java/com/mobikwik/standingrequest/controller/RequestController.java
+++ football/src/main/java/com/mobikwik/standingrequest/controller/RequestController.java
@@ -5,6 +5,7 @@ import com.mobikwik.standingrequest.model.Item;
 import com.mobikwik.standingrequest.model.League;
 import com.mobikwik.standingrequest.model.TeamPosition;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +16,19 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("/standing")
 public class RequestController {
+    @Value("${country}")
+    private String countryUrl;
+    @Value("${league}")
+    private String leagueUrl;
+    @Value(("${team}"))
+    private String teamUrl;
     @Autowired
     private RestTemplate restTemplate;
     @RequestMapping("/{teamName}/{leagueName}/{countryName}")
     public Item getItem(@PathVariable("teamName") String teamName, @PathVariable("leagueName") String leagueName, @PathVariable("countryName") String countryName){
-        Country country = restTemplate.getForObject("http://localhost:9092/country/"+countryName, Country.class);
-        League league = restTemplate.getForObject("http://localhost:8081/league/"+leagueName+"/"+country.getCountryId(), League.class);
-        TeamPosition teamPosition = restTemplate.getForObject("http://localhost:9093/team/"+teamName+"/"+league.getLeagueId(), TeamPosition.class);
+        Country country = restTemplate.getForObject(countryUrl+countryName, Country.class);
+        League league = restTemplate.getForObject(leagueUrl+leagueName+"/"+country.getCountryId(), League.class);
+        TeamPosition teamPosition = restTemplate.getForObject(teamUrl+teamName+"/"+league.getLeagueId(), TeamPosition.class);
         Item item = new Item(countryName,country.getCountryId(),leagueName,league.getLeagueId(),teamName,teamPosition.getTeamId(),teamPosition.getOverallPosition());
         return item;
     }
